cb0ce7fe20719559e8a0e6dfcc7e5852123908e5
Gaelle Maurieres <gmaurieres@slb.com>
2024-01-26 11:36:00 +0100

    services/game-content@feat: setup database

--- services/game-content/app/main.py
+++ services/game-content/app/main.py
@@ -1,6 +1,22 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from .api.v0_router import v0_router
+from .persistence.engine import create_db_and_tables
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    create_db_and_tables()
+    yield
+
+
+app = FastAPI(
+    title="Nacon interview project.",
+    description="Mini game content manager.",
+    lifespan=lifespan,
+)
 
 
 @app.get("/")
